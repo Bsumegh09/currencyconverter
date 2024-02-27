45858a5786d86736dd7e5b1d0dfe6f6b644ed0c9
padakitanmay <padakitanmay@gmail.com>
2024-02-27 19:48:35 +0530

    Readme was updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import InputCard from "./components/InputCard";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
-// import "./App.css";
 import BackgroundImage from "/back.jpg";
 
 function App() {
